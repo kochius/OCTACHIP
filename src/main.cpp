bca7c1f2d83c40dc43fba9b9e47627aa0d51dc5c
kochius <132928838+kochius@users.noreply.github.com>
2023-05-18 19:11:41 -0700

    Move main into src directory

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "src/emulator.hpp"
+#include "emulator.hpp"
 
 #include <iostream>
 #include <random>
@@ -6,7 +6,7 @@
 int main() {
     CHIP8::Emulator chip8{};
     std::cout << "Loading ROM..." << "\n";
-    chip8.loadRom("./roms/tetris.ch8");
+    chip8.loadRom("../roms/tetris.ch8");
     std::cout << "Running emulator..." << "\n";
     chip8.run(700);
     return EXIT_SUCCESS;
