59c877d51826601fa32752f31bbac9f9f6eb0006
kochius <132928838+kochius@users.noreply.github.com>
2023-05-23 18:05:00 -0700

    Add command line argument parsing with cxxopts

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,59 @@
+#include <cstdlib>
+#include <cxxopts.hpp>
+#include <exception>
+#include <filesystem>
 #include <iostream>
-#include <random>
+#include <string>
 
 #include "emulator.hpp"
 
-int main() {
-    CHIP8::Emulator chip8{};
-    std::cout << "Loading ROM..." << "\n";
-    chip8.loadRom("../roms/tetris.ch8");
-    std::cout << "Running emulator..." << "\n";
-    chip8.run(700);
+std::filesystem::path parsePath(const cxxopts::ParseResult& result);
+int parseSpeed(const cxxopts::ParseResult& result);
+
+int main(int argc, char* argv[]) {
+    cxxopts::Options options("chip8", "A CHIP-8 emulator written in C++");
+    options.add_options()
+        ("h,help", "Print usage")
+        ("r,rom", "ROM file path (*.ch8)", cxxopts::value<std::string>())
+        ("s,speed", "Emulation speed (in ticks per second)", 
+            cxxopts::value<int>()->default_value("700"));
+    
+    try {
+        cxxopts::ParseResult result = options.parse(argc, argv);
+
+        if (result.count("help")) {
+            std::cout << options.help() << "\n";
+            return EXIT_SUCCESS;
+        }
+
+        std::filesystem::path romPath = parsePath(result);
+        if (romPath.empty()) {
+            std::cerr << "No ROM file path provided\n";
+            std::cerr << "Usage: ./chip8 -r <PATH>\n";
+            return EXIT_FAILURE;
+        }
+
+        int ticksPerSecond = parseSpeed(result);
+
+        CHIP8::Emulator chip8{};
+        chip8.loadRom(romPath);
+        chip8.run(ticksPerSecond);
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << "\n";
+        return EXIT_FAILURE;
+    }
+    
     return EXIT_SUCCESS;
+}
+
+std::filesystem::path parsePath(const cxxopts::ParseResult& result) {
+    if (result.count("rom")) {
+        return std::filesystem::path{result["rom"].as<std::string>()};
+    }
+    return std::filesystem::path{};
+}
+
+int parseSpeed(const cxxopts::ParseResult& result) {
+    return result["speed"].as<int>();
 }
