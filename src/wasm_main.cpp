d27f831a0cac7131bd4bd9dd3330d56635674548
kochius <132928838+kochius@users.noreply.github.com>
2023-06-02 20:31:25 -0700

    Add ability to stop, pause, and resume emulator

--- src/wasm_main.cpp
+++ src/wasm_main.cpp
@@ -1,45 +1,40 @@
-#include <chrono>
 #include <emscripten.h>
-#include <string>
 
 #include "wasm_emulator.hpp"
 
-CHIP8::Emulator chip8{};
+static constexpr int defaultWindowScale = 15;
+static constexpr int defaultEmulationSpeed = 700;
 
-void loadRom(const std::string& romPath) {
-    chip8.interpreter.loadRom(romPath);
+CHIP8::Emulator chip8{defaultWindowScale, defaultEmulationSpeed};
+
+void loadRom(const char* romPath) {
+    chip8.loadRom(romPath);
+}
+
+extern "C" void stop() {
+    emscripten_cancel_main_loop();
+    chip8.reset();
+}
+
+extern "C" void pause() {
+    emscripten_pause_main_loop();
+}
+
+extern "C" void resume() {
+    chip8.refreshUpdateTimer();
+    emscripten_resume_main_loop();
 }
 
 void mainLoop() {
-    static double accumulator = 0.0;
-    static auto previousTime = std::chrono::high_resolution_clock::now();
-
-    const auto currentTime = std::chrono::high_resolution_clock::now();
-    const double deltaTime = std::chrono::duration<double>(currentTime - 
-        previousTime).count();
-    previousTime = currentTime;
-
-    accumulator += deltaTime;
-
-    while (accumulator >= CHIP8::Emulator::DELTA_TIME) {
-        accumulator -= CHIP8::Emulator::DELTA_TIME;
-
-        for (int i = 0; i < CHIP8::Emulator::INTSTRUCTIONS_PER_UPDATE; i++) {
-            chip8.interpreter.tick();
-        }
-
-        chip8.interpreter.updateTimers();
-    }
-
-    chip8.renderer.drawFrame(chip8.interpreter.getFrame());
-
-    chip8.input.processInput([&](const int key, const bool isPressed) {
-        chip8.interpreter.setKey(key, isPressed);
-    });
+    chip8.update();
 }
 
-int main() {
-    loadRom("roms/tetris.ch8");
-    emscripten_set_main_loop(mainLoop, 0, 1);
+extern "C" int main() {
+    chip8.refreshUpdateTimer();
+
+    const char* romPath = "roms/tetris.ch8";
+    chip8.loadRom(romPath);
+    emscripten_set_main_loop(mainLoop, 0, 0);
+
     return EXIT_SUCCESS;
 }
