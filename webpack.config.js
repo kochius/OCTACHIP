9296b6ebc81acd90a92d942b0deda2b501f539de
kochius <132928838+kochius@users.noreply.github.com>
2023-10-16 18:38:34 -0700

    Add Babel loader for code transpilation

--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
         test: /\.(css|sass|scss)$/,
         use: ["css-loader", "sass-loader"],
       },
